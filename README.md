4510ad9dc58741989e2a9e8b0448070e3d9f0a2d
pujanban <grunze21@gmail.com>
2021-01-27 20:02:17 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-kmgvfh
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-kmgvfh)
---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React, { createContext, useContext, useState } from "react";
+import "./style.css";
+
+export default function App() {
+  const ThemeContext = createContext(null);
+  const ThemeProvider = props => {
+    const [theme, setTheme] = useState("White");
+
+    const changeTheme = () => {
+      setTheme(prev => (prev === "White" ? "Black" : "White"));
+    };
+
+    return (
+      <ThemeContext.Provider value={{ theme, changeTheme }}>
+        {props.children}
+      </ThemeContext.Provider>
+    );
+  };
+
+  const CellOne = () => {
+    const { theme } = useContext(ThemeContext);
+    return <div className={`${theme} Base`}>This is {theme} color for Cell One.</div>;
+  };
+
+  const CellTwo = () => {
+    const { theme } = useContext(ThemeContext);
+    return <div className={`${theme} Base`}>This is {theme} color for Cell Two.</div>;
+  };
+
+  const ChangeTheme = () => {
+    const { changeTheme } = useContext(ThemeContext);
+    const onChangeTheme = () => {
+      changeTheme();
+    };
+    return <button onClick={onChangeTheme} className="Button">Toggle Theme </button>;
+  };
+
+  return (
+    <ThemeProvider>
+      <div className="Wrapper">
+        <div className="CellWrapper">
+          <CellOne />
+          <CellTwo />
+        </div>
+        <ChangeTheme/>
+      </div>
+    </ThemeProvider>
+  );
+}
